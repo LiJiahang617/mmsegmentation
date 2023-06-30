f4d1ba3343426a848915cd4622aa36d5122f8e7b
ljh <1471173088@qq.com>
2023-06-30 21:52:47 +0800

    2023.6.30 finished multimodal feature map visualization, but the name in wandb is not refactor correctly, need to do this tomorrow.

--- configs/twinformer/twinformerbase_convnext-xl_carla-352x640.py
+++ configs/twinformer/twinformerbase_convnext-xl_carla-352x640.py
@@ -2,7 +2,7 @@ _base_ = [
     '../_base_/default_runtime.py', '../_base_/datasets/mmcarla_640x352.py'
 ]
 
-pretrained = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/swin/swin_base_patch4_window12_384_20220317-55b0104a.pth'  # noqa
+pretrained = 'https://download.openmmlab.com/mmclassification/v0/convnext/downstream/convnext-xlarge_3rdparty_in21k_20220301-08aa5ddc.pth'
 
 crop_size = (352, 640) # h, w
 data_preprocessor = dict(
@@ -15,34 +15,22 @@ data_preprocessor = dict(
     size=crop_size)
 num_classes = 3
 
-depths = [2, 2, 18, 2]
 model = dict(
     type='EncoderDecoder',
     data_preprocessor=data_preprocessor,
     backbone=dict(
-        type='TwinSwinTransformer',
-        pretrain_img_size=384,
-        # normal has 3 channels, but depth, tdisp, disp have 1 channels,
-        # not sure if it is suitable to force them to have 3 uniformly.
-        in_channels=3,
-        embed_dims=128,
-        depths=depths,
-        num_heads=[4, 8, 16, 32],
-        window_size=12,
-        mlp_ratio=4,
-        qkv_bias=True,
-        qk_scale=None,
-        drop_rate=0.,
-        attn_drop_rate=0.,
-        drop_path_rate=0.3,
-        patch_norm=True,
-        out_indices=(0, 1, 2, 3),
-        with_cp=False,
-        frozen_stages=-1,
-        init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
+        type='mmpretrain.TwinConvNeXt',
+        arch='xlarge',
+        out_indices=[0, 1, 2, 3],
+        drop_path_rate=0.4,
+        layer_scale_init_value=1.0,
+        gap_before_final_norm=False,
+        init_cfg=dict(
+            type='Pretrained', checkpoint=pretrained,
+            prefix='backbone.')),
     decode_head=dict(
         type='TwinFormerHead',
-        in_channels=[256, 512, 1024, 2048], # modified here
+        in_channels=[256, 512, 1024, 2048],  # modified here
         strides=[4, 8, 16, 32],
         feat_channels=256,
         out_channels=256,
@@ -149,53 +137,13 @@ model = dict(
     train_cfg=dict(),
     test_cfg=dict(mode='whole'))
 
-# set all layers in backbone to lr_mult=0.1
-# set all norm layers, position_embeding,
-# query_embeding, level_embeding to decay_multi=0.0
-backbone_norm_multi = dict(lr_mult=0.1, decay_mult=0.0)
-backbone_embed_multi = dict(lr_mult=0.1, decay_mult=0.0)
-embed_multi = dict(lr_mult=1.0, decay_mult=0.0)
-custom_keys = {
-    'backbone': dict(lr_mult=0.1, decay_mult=1.0),
-    'backbone.patch_embed_x.norm': backbone_norm_multi,
-    'backbone.patch_embed_y.norm': backbone_norm_multi,
-    'backbone.norm': backbone_norm_multi,
-    'absolute_pos_embed': backbone_embed_multi,
-    'relative_position_bias_table': backbone_embed_multi,
-    'query_embed': embed_multi,
-    'query_feat': embed_multi,
-    'level_embed': embed_multi
-}
-# for RGB encoder
-custom_keys.update({
-    f'backbone.stages_x.{stage_id}.blocks.{block_id}.norm': backbone_norm_multi
-    for stage_id, num_blocks in enumerate(depths)
-    for block_id in range(num_blocks)
-})
-# for another encoder
-custom_keys.update({
-    f'backbone.stages_y.{stage_id}.blocks.{block_id}.norm': backbone_norm_multi
-    for stage_id, num_blocks in enumerate(depths)
-    for block_id in range(num_blocks)
-})
-# for RGB encoder
-custom_keys.update({
-    f'backbone.stages_x.{stage_id}.downsample.norm': backbone_norm_multi
-    for stage_id in range(len(depths) - 1)
-})
-# for another encoder
-custom_keys.update({
-    f'backbone.stages_y.{stage_id}.downsample.norm': backbone_norm_multi
-    for stage_id in range(len(depths) - 1)
-})
 # optimizer
 optimizer = dict(
     type='AdamW', lr=0.0001, weight_decay=0.05, eps=1e-8, betas=(0.9, 0.999))
 optim_wrapper = dict(
     type='OptimWrapper',
     optimizer=optimizer,
-    clip_grad=dict(max_norm=0.01, norm_type=2),
-    paramwise_cfg=dict(custom_keys=custom_keys, norm_decay_mult=0.0))
+    clip_grad=dict(max_norm=5.0))
 
 # learning policy
 param_scheduler = [
@@ -204,7 +152,7 @@ param_scheduler = [
         eta_min=0,
         power=0.9,
         begin=0,
-        end=160000,
+        end=50,
         by_epoch=True)
 ]
 
@@ -221,10 +169,4 @@ default_hooks = dict(
         type='CheckpointHook', by_epoch=True, interval=5,
         save_best='mIoU'),
     sampler_seed=dict(type='DistSamplerSeedHook'),
-    visualization=dict(type='SegVisualizationHookplus', draw=True))
-
-# Default setting for scaling LR automatically
-#   - `enable` means enable scaling LR automatically
-#       or not by default.
-#   - `base_batch_size` = (8 GPUs) x (2 samples per GPU).
-# auto_scale_lr = dict(enable=False, base_batch_size=3)
+    visualization=dict(type='SegVisualizationHook', draw=True))
---
+++ feature_map_visual.py
@@ -0,0 +1,204 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from argparse import ArgumentParser
+from typing import Type, Union, Sequence
+from collections import defaultdict
+
+import numpy as np
+import mmcv
+import torch
+import torch.nn as nn
+
+from mmengine.dataset import Compose
+from mmengine.model import revert_sync_batchnorm
+from mmengine.structures import PixelData
+from mmseg.apis import inference_model, init_model
+from mmseg.structures import SegDataSample
+from mmseg.utils import register_all_modules
+from mmseg.visualization import SegLocalVisualizer
+from mmseg.models import BaseSegmentor
+from mmseg.utils import SampleList
+
+
+class Recorder:
+    """record the forward output feature map and save to data_buffer."""
+
+    def __init__(self) -> None:
+        self.data_buffer = list()
+
+    def __enter__(self, ):
+        self._data_buffer = list()
+
+    def record_data_hook(self, model: nn.Module, input: Type, output: Type):
+        self.data_buffer.append(output)
+
+    def __exit__(self, *args, **kwargs):
+        pass
+
+
+
+def _preprare_mulitmodal_data(imgs, anos, model):
+
+    cfg = model.cfg
+    for t in cfg.test_pipeline:
+        if t.get('type') in ['LoadAnnotations', 'LoadCarlaAnnotations']:
+            cfg.test_pipeline.remove(t)
+
+    is_batch = True
+    if not isinstance(imgs, (list, tuple)):
+        imgs = [imgs]
+        anos = [anos]
+        is_batch = False
+
+    if isinstance(imgs[0], np.ndarray):
+        cfg.test_pipeline[0]['type'] = 'LoadImageFromNDArray'
+
+    # TODO: Consider using the singleton pattern to avoid building
+    # a pipeline for each inference
+    pipeline = Compose(cfg.test_pipeline)
+
+    data = defaultdict(list)
+    for (img, ano) in zip(imgs, anos):
+        if isinstance(img, np.ndarray) and isinstance(ano, np.ndarray):
+            data_ = dict(img=img)
+            data_['ano'] = ano
+        else:
+            data_ = dict(img_path=img)
+            data_['ano_path'] = ano
+        data_ = pipeline(data_)
+        data['inputs'].append(data_['inputs'])
+        data['data_samples'].append(data_['data_samples'])
+
+    return data, is_batch
+
+ImageType = Union[str, np.ndarray, Sequence[str], Sequence[np.ndarray]]
+
+def inference_multimodel(img: ImageType, ano: ImageType,
+                         model: BaseSegmentor) -> Union[SegDataSample, SampleList]:
+    """Inference image(s) with the segmentor.
+
+    Args:
+        model (nn.Module): The loaded segmentor.
+        imgs (str/ndarray or list[str/ndarray]): Either image files or loaded
+            images.
+
+    Returns:
+        :obj:`SegDataSample` or list[:obj:`SegDataSample`]:
+        If imgs is a list or tuple, the same length list type results
+        will be returned, otherwise return the segmentation results directly.
+    """
+    # prepare data
+    data, is_batch = _preprare_mulitmodal_data(img, ano, model)
+
+    # forward the model
+    with torch.no_grad():
+        results = model.test_step(data)
+
+    return results if is_batch else results[0]
+
+def visualize(args, model, recorder, result):
+    seg_visualizer = SegLocalVisualizer(
+        vis_backends=[dict(type='WandbVisBackend')],
+        save_dir='temp_dir',
+        alpha=0.5)
+    seg_visualizer.dataset_meta = dict(
+        classes=model.dataset_meta['classes'],
+        palette=model.dataset_meta['palette'])
+
+    image = mmcv.imread(args.img, 'color')
+
+    # prepare data
+    # data, is_batch = _preprare_mulitmodal_data(args.img, args.ano, model)
+    # seg_visualizer.add_graph(model, data)
+
+    seg_visualizer.add_datasample(
+        name='predict',
+        image=image,
+        data_sample=result,
+        draw_gt=False,
+        draw_pred=True,
+        wait_time=0,
+        out_file=None,
+        show=False)
+
+    # add feature map to wandb visualizer
+    for i in range(len(recorder.data_buffer)):
+        feature = recorder.data_buffer[i][0]  # remove the batch
+        drawn_img = seg_visualizer.draw_featmap(
+            feature, image, channel_reduction='select_max')
+        seg_visualizer.add_image(f'feature_map{i}', drawn_img)
+
+    if args.gt_mask:
+        sem_seg = mmcv.imread(args.gt_mask, 'unchanged')
+        sem_seg = torch.from_numpy(sem_seg)
+        gt_mask = dict(data=sem_seg)
+        gt_mask = PixelData(**gt_mask)
+        data_sample = SegDataSample()
+        data_sample.gt_sem_seg = gt_mask
+
+        seg_visualizer.add_datasample(
+            name='gt_mask',
+            image=image,
+            data_sample=data_sample,
+            draw_gt=True,
+            draw_pred=False,
+            wait_time=0,
+            out_file=None,
+            show=False)
+
+    seg_visualizer.add_image('image', image)
+
+
+def main():
+    parser = ArgumentParser(
+        description='Draw the Feature Map During Inference')
+    parser.add_argument('img', help='Image file')
+    parser.add_argument('ano', help='Another file')
+    parser.add_argument('config', help='Config file')
+    parser.add_argument('checkpoint', help='Checkpoint file')
+    parser.add_argument('--gt_mask', default=None, help='Path of gt mask file')
+    parser.add_argument('--out-file', default=None, help='Path to output file')
+    parser.add_argument(
+        '--device', default='cuda:0', help='Device used for inference')
+    parser.add_argument(
+        '--opacity',
+        type=float,
+        default=0.5,
+        help='Opacity of painted segmentation map. In (0, 1] range.')
+    parser.add_argument(
+        '--title', default='result', help='The image identifier.')
+    args = parser.parse_args()
+
+    register_all_modules()
+
+    # build the model from a config file and a checkpoint file
+    model = init_model(args.config, args.checkpoint, device=args.device)
+    if args.device == 'cpu':
+        model = revert_sync_batchnorm(model)
+
+    # show all named module in the model and use it in source list below
+    for name, module in model.named_modules():
+        print(name)
+
+    source = ['decode_head.pixel_decoder.output_convs.0.activate'
+    ]
+    source = dict.fromkeys(source)
+
+    count = 0
+    recorder = Recorder()
+    # registry the forward hook
+    for name, module in model.named_modules():
+        if name in source:
+            count += 1
+            module.register_forward_hook(recorder.record_data_hook)
+            if count == len(source):
+                break
+
+    with recorder:
+        # test a single image, and record feature map to data_buffer
+        result = inference_multimodel(args.img, args.ano, model)
+
+    visualize(args, model, recorder, result)
+
+
+if __name__ == '__main__':
+    main()
